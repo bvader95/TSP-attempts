2a4abc86653ebfb415489f0e82675c4095bf6bbd
Bartlomiej W. (PCMR) <B.I.Weclewski@gmail.com>
2018-09-09 18:58:52 +0200

    First initial attempts at measuring execution time using std::chrono

--- TSP attempts/main.cpp
+++ TSP attempts/main.cpp
@@ -2,11 +2,15 @@
 #include "PointTSP.h"
 
 int main() {
-	PointTSP tsp("westernsahara29.txt");
-	TSPSolution sol = tsp.localSearch();
+	PointTSP tsp("burma14.txt");
+	auto start = std::chrono::steady_clock::now();
+	TSPSolution sol = tsp.branchAndBound();
+	auto stop = std::chrono::steady_clock::now();
+	auto timeInMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
 	std::cout << sol.value<<"\t";
 	for (unsigned int i = 0; i < sol.path.size(); ++i)std::cout << sol.path[i]<<" ";
 	std::cout << std::endl;
+	std::cout << "Time in ms: " << timeInMilliseconds << std::endl;
 	system("pause");
 	return 0;
 }
